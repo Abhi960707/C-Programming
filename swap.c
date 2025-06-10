e56444b3fff34cf6c209e4db8e95e552bf6561a1
Abhijeet narsale <abhijeetnarsale14082004@gmail.com>
2025-06-10 22:24:34 +0530

    substraction

---
+++ swap.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,temp;
+    a=10;
+    b=20;
+
+    printf("before swaping %d %d \n",a,b);
+
+    temp=a;
+    a=b;
+    b=temp;
+
+    printf("after swaping %d %d \n",a,b);
+
+   
+ 
+ return 0;
+
+
+}
