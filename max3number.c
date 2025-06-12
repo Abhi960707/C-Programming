55828b1d8a75f705ef051ab3ac630fe13c8238a5
Abhijeet narsale <abhijeetnarsale14082004@gmail.com>
2025-06-12 13:25:15 +0530

    max 3 number

--- max3number.c
+++ max3number.c
@@ -4,11 +4,11 @@ int main()
     int a,b,c;
 printf("enter 3 number:");
 scanf("%d%d%d",&a,&b,&c);
-if(a>b && a>c)
+if(a>=b && a>c)
 printf("max is%d",a);
 else if(b>a && b>c)
 printf("max is%d",b);
-else 
+else
 printf("max is%d",c);
 
 return 0;
