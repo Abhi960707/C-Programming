54b1d0316c0decff926d568a20339538ec1df577
Abhijeet narsale <abhijeetnarsale14082004@gmail.com>
2025-06-26 21:14:50 +0530

    prime number

---
+++ primeNo2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,cnt=0;
+    printf("enter a nmmbe ");
+    scanf("%d",&n);
+
+   cnt=0;
+for ( i = 2; i <=n/2; i++)
+{
+    if (n%i==0)
+    cnt++;
+
+}
+if (cnt==0)
+printf("prime number ");
+else
+printf("not prime number ");
+
+
+    return 0;
+
+}
