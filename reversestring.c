04972c0e87043ad96abbfd40510dbd5bc4c7d2e7
Abhijeet narsale <abhijeetnarsale14082004@gmail.com>
2025-06-27 16:18:33 +0530

    fibonacci reverse

---
+++ reversestring.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char num[100];
+    printf("enter number :");
+    scanf("%s",&num);
+
+    strrev(num);
+    printf("reverse no : %s",num);
+    return 0;
+    
+}
