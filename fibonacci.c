aea98db0d2cc2b01011c632992b858f972c7bf3a
Abhijeet narsale <abhijeetnarsale14082004@gmail.com>
2025-07-02 14:19:00 +0530

    array

--- fibonacci.c
+++ fibonacci.c
@@ -2,7 +2,7 @@
 int main()
 {
     int a=0,b=1,c,i,n;
-    printf("enter a nunmber :");
+   if(printf("enter number "))
     scanf("%d",&n);
 
     if (n==1)
---
+++ maxarray.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a[100],n,i,max;
+    printf(" enter a number  :");
+    scanf("%d",&n);
+    max=a[0];
+     if(max<a[i])
+     max=a[i];
+     max=(max <a[i])?a[i]:max;
+     max=fmax(a[i],max);
+
+
+     return 0;
+}
