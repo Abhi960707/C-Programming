5c18d7ab8b271bd831d4d5f871981d7779022ed9
Abhijeet narsale <abhijeetnarsale14082004@gmail.com>
2025-07-04 22:47:05 +0530

    sorting

--- bubblesort.c
+++ bubblesort.c
@@ -7,12 +7,21 @@ int main()
 
     printf("enter %d element ",n);
     for ( i = 0; i < n; i++)
+    scanf("%d",&a[i]);
+       for ( i = 0; i < n; i++)
+       {
+    for ( j= 0; j <n-1-i; j++)
     {
-        /* code */
+        if (a[j]>a[j+1])
+        {
+            int temp=a[j];
+            a[j]=a[j+1];
+            a[j+1]=temp;
+        }
     }
-    
-
-
+    }
+    for ( i = 0; i <n; i++)
+    printf("\t%d",a[i]);
 
     return 0;
 }
